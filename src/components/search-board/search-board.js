098346440092554a3ec1e814d1be431aef2d33e2
vmerzlyakov <merzliakov-v@yandex.ru>
2021-06-08 14:44:19 +0300

    fix search-board style

--- src/components/search-board/search-board.js
+++ src/components/search-board/search-board.js
@@ -5,9 +5,16 @@ function SearchBoard() {
         <div className="search-board-wrapper">
             <input
                 className="search-board"
-                placeholder="Начните вводить"
+                placeholder="Начните вводить..."
                 key="searchBoard"
             />
+            <div className="magnifier">
+                <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="8.05263" cy="8.1579" r="7.05263" stroke="#C1C1C1" strokeWidth="2"/>
+                    <line x1="12.3387" y1="12.8192" x2="17.7071" y2="18.1876" stroke="#C1C1C1" strokeWidth="2"/>
+                </svg>
+            </div>
+
         </div>
     );
 }
