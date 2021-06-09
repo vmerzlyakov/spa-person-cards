05a9a78b4967fa4ccb07608b124dd837c2d4d785
vmerzlyakov <merzliakov-v@yandex.ru>
2021-06-09 18:34:41 +0300

    add headers for filters, fix placeholder styles

--- src/components/filters/filters-by-gender.js
+++ src/components/filters/filters-by-gender.js
@@ -3,21 +3,26 @@ import React from 'react';
 import { filtersByGender } from '../../constants/filter';
 
 function FilterByGender({ checkedFilter, handleOnChange }) {
-    return <div className="filter by-gender">
-        { filtersByGender.map(filter => (
-            <label className="radio" key={ filter.code }>
-                <input
-                    className="input"
-                    type="radio"
-                    name="sex"
-                    checked={ filter.code === checkedFilter }
-                    onChange={ () => handleOnChange(filter.code) }
-                />
-                <div className="radio-marker"></div>
-                { filter.name }
-            </label>)
-        ) }
-    </div>;
+    return (
+        <div>
+            <div className="filter-header">Фильтр по полу</div>
+            <div className="filter by-gender">
+                { filtersByGender.map(filter => (
+                    <label className="radio" key={ filter.code }>
+                        <input
+                            className="input"
+                            type="radio"
+                            name="sex"
+                            checked={ filter.code === checkedFilter }
+                            onChange={ () => handleOnChange(filter.code) }
+                        />
+                        <div className="radio-marker"></div>
+                        { filter.name }
+                    </label>)
+                ) }
+            </div>
+        </div>
+    );
 }
 
 
